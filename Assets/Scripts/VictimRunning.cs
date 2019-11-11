1cee6111f58bc2f2b026e4f1b842f94aa0573bc1
tinokun <smith.antonio@gmail.com>
2019-11-10 18:05:00 -0600

    victim now attempts to escape when the zombie gets close

---
+++ Assets/Scripts/VictimRunning.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictimRunning : MonoBehaviour
+{
+
+    public float escapeSpeed;
+
+    public float speedDecreaser;
+    public float speedDecreaseMilestone;
+    private float speedMilestoneCount;
+
+    public bool grounded;
+    public LayerMask groundLayer;
+
+    private Collider2D groundCollider;
+
+    private Rigidbody2D victimRigidBody;
+
+    private GameObject theZombie;
+    private Vector2 zombiePosition;
+    private Vector3 victimPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        victimRigidBody = GetComponent<Rigidbody2D>();
+        groundCollider = GetComponent<Collider2D>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theZombie = GameObject.Find("Zombie");
+        zombiePosition = theZombie.transform.position;
+        victimPosition = transform.position;
+        if ((victimPosition.x - zombiePosition.x) < 10)
+        {
+            Runaway();
+        }
+    }
+
+    void Runaway()
+    {
+        grounded = Physics2D.IsTouchingLayers(groundCollider, groundLayer);
+
+        if (transform.position.x > speedMilestoneCount)
+        {
+            speedMilestoneCount += speedDecreaseMilestone;
+            escapeSpeed = escapeSpeed * speedDecreaser;
+        }
+
+        victimRigidBody.velocity = new Vector2(escapeSpeed, victimRigidBody.velocity.y);
+    }
+
+    
+}
